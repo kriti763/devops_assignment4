4802175b244af215d22ab81d399610a21dcf1af5
Kriti <2021wb86842@wilp.bits-pilani.ac.in>
2025-07-07 12:58:17 +0530

    Modified the App.java file

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App{
 	public static void main(String [] args){
-		System.out.println("This is my BITS DevOps Assignment4");
+		System.out.println("This is my BITS DevOps Assignment4 - Updated");
 	}
 }
